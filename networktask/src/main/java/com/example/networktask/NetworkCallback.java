f5660bd03130e6d7ca27566a029118cdffcb8f08
Ramon van Sprundel <ramonvansprundel@gmail.com>
2020-12-24 15:39:43 +0100

    fixed release to work with tag

--- networktask/src/main/java/com/example/networktask/NetworkCallback.java
+++
@@ -1,8 +0,0 @@
-package com.example.networktask;
-
-/**
- * Implement this callback with a lambda to use it
- */
-public interface NetworkCallback {
-    void callback(Object data, boolean success);
-}
